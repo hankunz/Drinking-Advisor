0feebe4ed313ad7ec85bb66e50246e163453f978
ch918372 <ch9183722@dal.ca>
2014-02-27 16:40:05 -0400

    Added Javascript Files

---
+++ JavaScript Codes/AddDrinks.js
@@ -0,0 +1,35 @@
+function addToWeek()
+{
+	//At the end of the day, add to the week
+	if(week.length()<=7)
+		week.push(total);
+	else
+	{	
+		week = [];
+		week.push(total);
+	}
+}
+
+
+
+function addToToday(justDrank)
+{
+	if (total.day==null)
+	{
+		total.day = justDrank.today;
+		total.alcohol = justDrank.alcohol;
+		total.calories = justDrank.calories;
+		total.sugar = justDrank.sugar;
+		total.price = justDrank.price;
+		
+	}
+	else
+	{
+		total.day += justDrank.today;
+		total.alcohol += justDrank.alcohol;
+		total.calories += justDrank.calories;
+		total.sugar += justDrank.sugar;
+		total.price += justDrank.price;
+		
+	}
+}
