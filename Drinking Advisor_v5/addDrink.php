d2d89feeab412df8cee05c62e33fbb1cd0abed20
Hankun Zhang <zhanghkun@gmail.com>
2014-03-27 21:26:36 -0300

    New adding drink page
    The newest version with new adding drink page.

---
+++ Drinking Advisor_v5/addDrink.php
@@ -0,0 +1,182 @@
+<?php ob_start(); 
+ // start up your PHP session!
+ session_start();
+  ?>
+<!-- This page is the main UI of the application. It provides a side navigation bar to switch from different pages and also a main page to keep track of 
+ 	users' daily drinking status.	-->
+<!DOCTYPE HTML>
+<head>
+<title>Drinking Advisor</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="shortcut icon" href="../favicon.ico"> 
+		<link rel="stylesheet" type="text/css" href=" css/default.css" />
+		<link rel="stylesheet" type="text/css" href=" css/multilevelmenu.css" />
+		<link rel="stylesheet" type="text/css" href=" css/component.css" />
+		<link rel="stylesheet" type="text/css" href=" css/animations.css" />
+		<script src=" js/modernizr.custom.js"></script>
+								<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src=" js/jquery.dlmenu.js"></script>
+		<script src=" js/pagetransitions.js"></script>
+	  
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href=" css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<script type="text/javascript" src=" js/jquery.js"></script>
+<script type="text/javascript" src=" js/login.js"></script>
+<script type="text/javascript" src=" js/Chart.js"></script>
+ <script type="text/javascript" src=" js/jquery.easing.js"></script>
+ <script type="text/javascript" src=" js/jquery.ulslide.js"></script>
+
+  <link type="text/css" href=" css/mmenu.css" rel="stylesheet" media="all" />
+<script type="text/javascript" src=" js/jquery.mmenu.min.js"></script>
+<script type="text/javascript">
+		$(function() {
+			$('nav#menu').mmenu();
+		});
+	</script>
+</head>
+<body>			
+
+
+
+<?php require 'php/dataRetrieval.php';?>
+<?php require 'php/drinkInsertion.php';?>
+<?php require 'php/drinkDataExport.php';?>
+
+<!-- main tag for the UI -->
+	    <div class="wrap">	 
+	      <div class="header">
+	      	  <div class="header_top">
+	      	     	<div class="menu-ico"><a href="#menu"></a></div>
+					  <div class="profile_details">
+				    		   <div id="loginContainer">
+				                  <a id="loginButton" class=""><span>Me</span></a>   
+				                    <div id="loginBox">                
+				                     
+				                     
+				                     
+				                     <form id="loginForm" name="loginForm" >
+				                     
+				                      <!-- show user info. -->
+				                      
+				                        <fieldset id="body">
+				                            <div class="user-info">
+							        			<h4>Hello,<a href="#"> <?php if(isset($_SESSION['userId'])){echo $getNameQueryResultVal;}else{echo 'Username';}  ?></a></h4>
+							        			<ul>
+							        				<li class="profile active"><a href="profile.php">Profile </a></li>
+							        				
+	
+							        				  <li class="logout"><a href="index.php"> Logout</a></li> 
+							        		      
+   				
+							        				
+							        				
+							        				<div class="clear"></div>		
+							        			</ul>
+							        		</div>			                            
+				                        </fieldset>
+				                    </form>				                  
+				                
+				                </div>
+				            </div>
+				             <div class="profile_img">	
+				             	<img src=" images/logo.jpg"  alt="logo" class="logoPic" />	
+				             </div>		
+				             <div class="clear"></div>		  	
+					    </div>	
+		 		      <div class="clear"></div>				 
+				   </div>
+			</div>	  					     
+	   </div>	  			
+
+	
+	
+		<!-- The window that use a line chart to keep track of users daily drinking status -->
+ 	<div id="pt-main" class="pt-perspective">	
+ 	
+
+<!-- The page for adding a drink-->
+ 	
+ 	 <div class="wrap">  
+ 
+ 		 
+	       <div class="column_left">	
+
+				 <div class="column_right_grid sign-in">
+				 	<div class="sign_in">
+				       <h3>What did you just drink?</h3>
+				
+					    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+					    	<span>
+					 	    <i><img src=" images/likes.png" alt="" /></i>
+					 	    <input name="drinkName" type="text" value="Enter the name of the drink. E.g. Orange Juice" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter the name of the drink. E.g. Orange Juice';}">
+					 	    </span>
+					 	    <span>
+					 	     <i><img src=" images/statistics.png" alt="" /></i>
+					 	     <input name="drinkVolume" type="text" value="Enter the volume (ml.). E.g. 355" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter the volume (ml.). E.g. 355';}">
+					 	    </span>
+					 	    <span>
+					 	     <i><img src=" images/statistics.png" alt="" /></i>
+					 	     <input name="drinkCalories" type="text" value="Enter the drink's calories. E.g. 50" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter the drink\'s calories. E.g. 50';}">
+					 	    </span>
+					 	     <span>
+					 	     <i><img src=" images/statistics.png" alt="" /></i>
+					 	     <input name="drinkSugar" type="text" value="Enter the drink's sugar. E.g. 20" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter the drink\'s sugar. E.g. 20';}">
+					 	    </span>
+					 	    <span>
+					 	     <i><img src=" images/statistics.png" alt="" /></i>
+					 	     <input name="drinkAlcohol" type="text" value="Enter the drink's alcohol percentage. E.g. 5" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter the drink\'s alcohol percentage. E.g. 5';}">
+					 	    </span>
+					 	    <span>
+					 	     <i><img src=" images/statistics.png" alt="" /></i>
+					 	     <input name="drinkVitaminc" type="text" value="Enter the drink's Vitamin c. E.g. 3" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter the drink\'s Vitamin c. E.g. 3';}">
+					 	    </span>
+					 	    
+					 	    </form>
+					 	
+
+
+				   </div>
+				 
+
+             </div>
+    	
+ 	 </div>
+	  </div>
+  	<div class="addDrink">
+					    <form>
+					 		<input class="iterateEffects" type="submit" value="Add!" >
+					 	</form>
+				   </div>
+				   
+
+		         
+				  
+				  
+</div>
+
+
+
+					<!-- The new side navigation bar-->
+   					<nav id="menu">
+							<ul>
+								<li><a href="main.php"><i><img src=" images/invites.png"></i>Home</a></li>
+								<li><a href="profile.php"><span><i><img src=" images/user.png"></i>Profile</span></a></li>
+						  		<li><a href="#"><span><i><img src=" images/statistics.png"></i>Statistics</span></a></li>		
+																							
+							</ul>
+					 </nav>
+				
+
+				    
+	
+		<script src=" js/jquery.dlmenu.js"></script>
+		<script src=" js/pagetransitions.js"></script>
+	
+
+</body>
+</html>
+
+<?php
+ob_end_flush();
+?>
